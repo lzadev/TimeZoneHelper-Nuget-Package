82bc5b1406bdd550df0d786c14ccca51df97619d
Luis Zabala <ldelorden@mardom.com>
2022-12-14 14:52:04 -0400

    initial

---
+++ TimeZoneHelper/TimeZoneHelper/DateTimeHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace TimeZoneHelper
+{
+    public static class DateTimeHelper
+    {
+        /// <summary>
+        /// This method convert the currentTime to a speficic TimeZone
+        /// </summary>
+        /// <param name="timeZoneId"></param>
+        /// <returns>A DateTime for the specific TimeZoneId provided</returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static DateTime GetDateTimeByTimeZoneId(string timeZoneId)
+        {
+            var timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+
+            if (timeZoneInfo == null) throw new ArgumentException($"Not TimeZoneInfo was founded with the id {timeZoneId}");
+
+            var current = TimeZone.CurrentTimeZone.ToUniversalTime(DateTime.Now);
+            return TimeZoneInfo.ConvertTimeFromUtc(current, timeZoneInfo);
+        }
+    }
+}
